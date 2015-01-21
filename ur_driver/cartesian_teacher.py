d7dd870feb32bf89b11e9d7947656c485be13815
abubeck <aub@ipa.fhg.de>
2015-01-21 09:55:36 +0100

    changed to own array name as states in issue #149

---
+++ ur_driver/cartesian_teacher.py
@@ -0,0 +1,17 @@
+header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: 'ur_base_link'
+poses:
+- position: {x: 0.4, y: 0.240, z: 0.4}
+  orientation: {x: 0.891, y: -0.453, z: -0.018, w: 0.014}
+- position: {x: 0.4, y: 0.240, z: 0.3}
+  orientation: {x: 0.891, y: -0.453, z: -0.018, w: 0.014}
+- position: {x: 0.4, y: 0.140, z: 0.3}
+  orientation: {x: 0.891, y: -0.453, z: -0.018, w: 0.014}
+- position: {x: 0.4, y: 0.140, z: 0.4}
+  orientation: {x: 0.891, y: -0.453, z: -0.018, w: 0.014}
+- position: {x: 0.4, y: 0.240, z: 0.4}
+  orientation: {x: 0.891, y: -0.453, z: -0.018, w: 0.014}
